13c94dbacf1266994b8227002dfa86cf68c22fee
zhangyaochun <zhangyaochun@mobike.com>
2017-06-25 10:36:23 +0800

    add(ensure)

--- require.ensure/index.js
+++ require.ensure/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
 
-require('./app.css');
-
-
 
 // render((
 //   <Router history={browserHistory}>
 //     <Route path="/" component={App}>
 //       <IndexRoute component={Dashboard}/>
 //       <Route path="app" component={Dashboard}/>
-//       <Route path="inbox" component={Inbox}/>
-//       <Route path="calendar" component={Calendar}/>
 //       <Route path="*" component={Dashboard}/>
 //     </Route>
 //   </Router>
